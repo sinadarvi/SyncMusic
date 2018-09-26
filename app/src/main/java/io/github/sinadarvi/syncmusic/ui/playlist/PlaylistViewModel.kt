092b7d3225039735ee60be2bb8a59b1afc7bf4c4
Sina Darvishi <sinadarvi@gmail.com>
2018-09-26 14:05:18 +0330

    Sync Music (flutter version) v0.1.0 - new branch added for flutter (experimental)

--- app/src/main/java/io/github/sinadarvi/syncmusic/ui/playlist/PlaylistViewModel.kt
+++
@@ -1,7 +0,0 @@
-package io.github.sinadarvi.syncmusic.ui.playlist
-
-import androidx.lifecycle.ViewModel;
-
-class PlaylistViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- nsd/src/main/java/io/github/sinadarvi/nsd/NsdType.kt
+++
@@ -1,9 +0,0 @@
-package io.github.sinadarvi.nsd
-
-/**
- * Typical NSD types.
- */
-object NsdType {
-    val HTTP = "_http._tcp."
-    val PRINTER = "_ipp._tcp."
-}
